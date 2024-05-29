352f789fe47f2c3731c7fcfb13ef1d4f90426747
kanochiiyo <andiniandrsta@gmail.com>
2024-05-29 19:16:52 +0700

    fix add and edit review (PLS NO BUG IM TIRED)

--- functions/handle_review.php
+++ functions/handle_review.php
@@ -1,11 +1,35 @@
 <?php
+session_start();
 require_once 'authentication.php';
 $connection = getConnection();
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-  $content = htmlspecialchars($_POST["reviewContent"]);
-  $rating = $_POST["reviewRating"];
+if (!isLogged()) {
+  header("Location: login.php");
+}
 
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['book_id']) && isset($_POST['reviewRating']) && isset($_POST['reviewContent'])) {
+  $bookId = $_POST['book_id'];
+  $rating = $_POST['reviewRating'];
+  $review = mysqli_real_escape_string($connection, $_POST['reviewContent']);
+  $reviewId = isset($_POST['review_id']) ? $_POST['review_id'] : 0;
+  $trxDetailId = $_POST['trxDetail_id'];
 
+  if ($reviewId > 0) {
+    // Update review
+    $query = "UPDATE review SET content = '$review', rate = $rating WHERE id = $reviewId";
+  } else {
+    // Insert new review
+    $query = "INSERT INTO review (content, rate) VALUES ('$review', $rating)";
+    if (mysqli_query($connection, $query)) {
+      $reviewId = mysqli_insert_id($connection);
+      $query = "UPDATE transaction_detail SET review_id = $reviewId WHERE id = $trxDetailId";
+    }
+  }
+
+  if (mysqli_query($connection, $query)) {
+    header("Location: ./../transaction.php");
+  } else {
+    echo "Error: " . mysqli_error($connection);
+  }
 }
 ?>
--- js/script.js
+++ js/script.js
@@ -59,3 +59,75 @@ document.addEventListener("DOMContentLoaded", function () {
     last.innerHTML = slider.value;
   });
 });
+
+// function untuk modal category
+document.addEventListener("DOMContentLoaded", function () {
+  const categoryModal = document.getElementById("categoryModal");
+  const dynamicFields = document.getElementById("dynamicFields");
+  const categoryTypeInput = document.getElementById("categoryType");
+
+  categoryModal.addEventListener("show.bs.modal", function (event) {
+    const button = event.relatedTarget;
+    const type = button.getAttribute("data-type");
+
+    categoryTypeInput.value = type;
+
+    let fields = "";
+    if (type === "author") {
+      fields = `
+          <div class="mb-3">
+            <label class="fw-bold" class="form-label" for="authorName">Author Name:</label>
+            <input type="text" class="form-control" id="authorName" name="authorName" required>
+          </div>
+        `;
+    } else if (type === "publisher") {
+      fields = `
+          <div class="mb-3">
+            <label class="fw-bold" class="form-label" for="publisherName">Publisher Name:</label>
+            <input type="text" class="form-control" id="publisherName" name="publisherName" required>
+          </div>
+        `;
+    } else if (type === "genre") {
+      fields = `
+          <div class="mb-3">
+            <label class="fw-bold" class="form-label" for="genreName">Genre Name:</label>
+            <input type="text" class="form-control" id="genreName" name="genreName"" required>
+          </div>
+        `;
+    }
+
+    dynamicFields.innerHTML = fields;
+  });
+});
+
+// function untuk edit review
+document.addEventListener("DOMContentLoaded", function () {
+  var reviewBtns = document.querySelectorAll(".reviewBtn");
+  reviewBtns.forEach(function (btn) {
+    btn.addEventListener("click", function () {
+      var bookTitle = this.getAttribute("data-book-title");
+      var author = this.getAttribute("data-author");
+      var bookId = this.getAttribute("data-book-id");
+      var trxDetailId = this.getAttribute("data-trx-id");
+      var reviewId = this.getAttribute("data-review-id");
+      var reviewContent = this.getAttribute("data-review-content");
+      var reviewRating = this.getAttribute("data-review-rating");
+
+      document.querySelector("#bookReviewModalLabel").textContent =
+        "Add Review for " + bookTitle + " by " + author;
+      document.querySelector('#bookReviewForm input[name="book_id"]').value =
+        bookId;
+      document.querySelector(
+        '#bookReviewForm input[name="trxDetail_id"]'
+      ).value = trxDetailId;
+      document.querySelector('#bookReviewForm input[name="review_id"]').value =
+        reviewId ? reviewId : "";
+      document.querySelector(
+        '#bookReviewForm textarea[name="reviewContent"]'
+      ).value = reviewContent ? reviewContent : "";
+      document.querySelector(
+        '#bookReviewForm input[name="reviewRating"]'
+      ).value = reviewRating ? reviewRating : "";
+    });
+  });
+});
