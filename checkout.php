8bd12b7f2b26f2031ac87fd579ef7a9ec2136c5d
kanochiiyo <andiniandrsta@gmail.com>
2024-05-31 14:20:18 +0700

    fix design co page. fix rowspan trx page

--- checkout.php
+++ checkout.php
@@ -69,22 +69,32 @@ include (__DIR__ . "/templates/navbar.php");
       <div class="col-5">
         <div class="card border-0">
           <div class="d-flex flex-column justify-content-center align-items-start p-5">
-            <div class="row">
-              <div class="col">
-                <h3 class="fw-bold">Total Amount</h3>
-                <h6>
-                  <?= $grand_total ?>
-                </h6>
+            <h1 class="fw-bold py-3">Order Summary</h1>
+            <div class="row w-100">
+              <div class="col-6">
+                <p>Total Price</p>
               </div>
+              <div class="col-6 d-flex justify-content-end">
+                <p>
+                  Rp <?= number_format($grand_total, 0, ',', '.') ?>
+                </p>
+              </div>
+              <div class="col-6">
+                <p>Delivery</p>
+              </div>
+              <div class="col-6 d-flex justify-content-end">
+                <p>
+                  Rp 0
+                </p>
+              </div>
+              <hr style="background: #000" class="mt-5 mb-2">
             </div>
-            <div class="row">
-              <div class="col-6">Delivery</div>
-            </div>
-            <hr style="background: red" class="mt-5 mb-2">
-            <a href="product.php" class="text-center links-bg-white mb-3" style="width: 100%; font-size:15px"> Continue
+            <span class="d-flex justify-content-end" style="width: 100%;">
+              <p class="fw-bold pe-3" style="font-size: 20px;">Rp <?= number_format($grand_total, 0, ',', '.') ?></p>
+            </span>
+            <a href="product.php" class="text-center links-co-white mb-3" style="width: 100%; font-size:15px"> Continue
               shopping </a>
-            <button type="submit" class="links-co" style="width: 100%;" form="checkoutForm">
-              Checkout </button>
+            <button type="submit" class="links-co" style="width: 100%;" form="checkoutForm"> Checkout </button>
           </div>
         </div>
       </div>
