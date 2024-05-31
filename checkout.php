ee3ffcb8121bc5658e9b7ed941863312d1e28c5b
kanochiiyo <andiniandrsta@gmail.com>
2024-05-31 13:15:21 +0700

    save changes

--- checkout.php
+++ checkout.php
@@ -26,90 +26,65 @@ $grand_total = 0;
 include (__DIR__ . "/templates/header.php");
 include (__DIR__ . "/templates/navbar.php");
 ?>
-<section class="d-flex justify-content-center font-inter align-items-center min-vh-100 max-vw-100"
-  style="padding-top:80px">
+<section id="checkout"
+  class="d-flex justify-content-center font-inter align-items-center min-vh-100 max-vw-100 font-poppins"
+  style="padding-top: 80px;">
   <div class="container">
-    <div class="card shadow-lg mt-5 mb-5">
-      <div class="row">
-        <div class="col-7 p-3 bg-light-brown">
-          <div class="card p-2 mb-2">
-            <div class="row">
-              <div class="col-6">
-                <p class="mb-0">Product</p>
-              </div>
-              <div class="col-2">
-                <p class="mb-0">Qty</p>
-              </div>
-              <div class="col-2">
-                <p class="mb-0">Price</p>
-              </div>
-              <div class="col-2">
-                <p class="mb-0">Subtotal</p>
-              </div>
-            </div>
-          </div>
-          <!-- card produk -->
+    <div class="row">
+      <div class="col-7">
+
+        <table class="table table-borderless">
+          <thead>
+            <tr>
+              <th colspan="2">Product</th>
+              <th>Qty</th>
+              <th>Price</th>
+              <th>Subtotal</th>
+            </tr>
+          </thead>
           <form action="functions/handle_checkout.php" method="post" id="checkoutForm">
-            <?php while ($book = $books->fetch_object()) {
-              $total = $book->qty * $book->price;
-              $grand_total += $total; ?>
-              <div class="card p-2 mb-2">
-                <div class="row">
-                  <div class="col-2">
-                    <img src="assets/books/<?= $book->img ?>" alt="book" class="rounded object-fit-cover"
-                      style="width: 75px;">
-                  </div>
-                  <div class="col-4">
-                    <p><?= $book->title ?></p>
-                    <p><?= $book->type ?></p>
-                  </div>
-                  <div class="col-2">
-                    <p><?= $book->qty ?></p>
-                  </div>
-                  <div class="col-2">
-                    <p><?= $book->price ?></p>
-                  </div>
-                  <div class="col-2">
-                    <p><?= $total ?></p>
-                  </div>
-                  <input type="hidden" name="qty[]" id="qty" value="<?= $book->qty ?>">
-                  <input type="hidden" name="book_id[]" id="book_id" value="<?= $book->book_id ?>">
-                  <input type="hidden" name="type[]" id="type" value="<?= $book->type ?>">
-                  <input type="hidden" name="id[]" id="id" value="<?= $book->id ?>">
-                </div>
-              </div>
-            <?php } ?>
+            <tbody>
+              <?php while ($book = $books->fetch_object()) {
+                $total = $book->qty * $book->price;
+                $grand_total += $total; ?>
+                <tr>
+                  <td><img src="assets/books/<?= $book->img ?>" alt="book" class="rounded object-fit-cover"
+                      style="width: 75px;"></td>
+                  <td><?= $book->title ?></td>
+                  <td><?= $book->qty ?></td>
+                  <td><?= $book->price ?></td>
+                  <td><?= $total ?></td>
+                </tr>
+                <input type="hidden" name="qty[]" value="<?= $book->qty ?>">
+                <input type="hidden" name="book_id[]" value="<?= $book->book_id ?>">
+                <input type="hidden" name="type[]" value="<?= $book->type ?>">
+                <input type="hidden" name="id[]" value="<?= $book->id ?>">
+              <?php } ?>
+            </tbody>
+
           </form>
-          <!-- end card produk -->
-        </div>
-        <div class="col-5 bg-dark-brown text-white">
+        </table>
+      </div>
+
+      <div class="col-5">
+        <div class="card border-0">
           <div class="d-flex flex-column justify-content-center align-items-start p-5">
-            <h1 class="fw-bold">Order Summary</h1>
-            <div class="row w-100">
-              <div class="col-6">
-                <p>Total Price</p>
-              </div>
-              <div class="col-6 d-flex justify-content-end">
-                <p>
+            <div class="row">
+              <div class="col">
+                <h3 class="fw-bold">Total Amount</h3>
+                <h6>
                   <?= $grand_total ?>
-                </p>
+                </h6>
               </div>
-              <div class="col-6">
-                <p>Delivery</p>
-              </div>
-              <div class="col-6 d-flex justify-content-end">
-                <p>
-                  $0
-                </p>
-              </div>
-              <hr style="background: #fff" class="mt-5 mb-2">
             </div>
-            <span class="d-flex justify-content-end" style="width: 100%;">
-              <p class="fw-bold" style="font-size: 20px"><?= $grand_total ?></p>
-            </span>
-            <a href="product.php" class="text-center links-co-white mb-3" style="width: 100%; font-size:15px"> Continue
+            <div class="row">
+              <div class="col-6">Delivery</div>
+            </div>
+            <hr style="background: red" class="mt-5 mb-2">
+            <a href="product.php" class="text-center links-bg-white mb-3" style="width: 100%; font-size:15px"> Continue
               shopping </a>
-            <button type="submit" class="links-co" style="width: 100%;" form="checkoutForm"> Checkout </button>
+            <button type="submit" class="links-co" style="width: 100%;" form="checkoutForm">
+              Checkout </button>
           </div>
         </div>
       </div>
